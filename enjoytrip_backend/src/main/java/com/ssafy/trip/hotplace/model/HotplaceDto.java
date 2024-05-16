0ceae259b3a094f1d2ce8806d1bf746ec417e27e
tjrl1225 <tjrl1225@naver.com>
2024-05-16 14:32:10 +0900

    fix: hotplace dto에 생성자, getter, setter 없는 문제 수정

--- enjoytrip_backend/src/main/java/com/ssafy/trip/hotplace/model/HotplaceDto.java
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/hotplace/model/HotplaceDto.java
@@ -16,4 +16,127 @@ public class HotplaceDto {
 	private int userId;
 	private String userUserId;
 	private String userNickname;
+	
+	public HotplaceDto() {
+		super();
+	}
+	
+	//join 없음
+	public HotplaceDto(int id, String title, String img, String description, String placeName, BigDecimal latitude,
+			BigDecimal longitude, int likeCount) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.img = img;
+		this.description = description;
+		this.placeName = placeName;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.likeCount = likeCount;
+	}
+
+	// join 있음
+	public HotplaceDto(int id, String title, String img, String description, String placeName, BigDecimal latitude,
+			BigDecimal longitude, int likeCount, int userId, String userUserId, String userNickname) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.img = img;
+		this.description = description;
+		this.placeName = placeName;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.likeCount = likeCount;
+		this.userId = userId;
+		this.userUserId = userUserId;
+		this.userNickname = userNickname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPlaceName() {
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+
+	public BigDecimal getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(BigDecimal latitude) {
+		this.latitude = latitude;
+	}
+
+	public BigDecimal getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(BigDecimal longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserUserId() {
+		return userUserId;
+	}
+
+	public void setUserUserId(String userUserId) {
+		this.userUserId = userUserId;
+	}
+
+	public String getUserNickname() {
+		return userNickname;
+	}
+
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
 }
