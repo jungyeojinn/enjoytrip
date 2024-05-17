88245406fc7aad3a6e4e01b35ea77645f82bb6f2
jinseok-kr <tjrl1225@gmail.com>
2024-05-17 16:37:14 +0900

    feat : jwt 로그인, 로그아웃, accesstoken 재발급 구현

--- enjoytrip_backend/src/main/java/com/ssafy/trip/user/model/service/UserService.java
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/user/model/service/UserService.java
@@ -2,14 +2,17 @@ package com.ssafy.trip.user.model.service;
 
 import java.sql.SQLException;
 
+import com.ssafy.trip.user.model.LoginResponse;
 import com.ssafy.trip.user.model.UserDto;
 
 public interface UserService {
 	void regi(UserDto user) throws SQLException;
 	
-	UserDto login(UserDto user) throws SQLException;
+	LoginResponse login(UserDto user) throws SQLException;
+	public void logout(String userId) throws SQLException;
 	UserDto getUser(String userId) throws SQLException;
 	void updateUser(UserDto user) throws SQLException;
 	void deleteUser(String userId) throws SQLException;
 	String getPassword(String userId) throws SQLException;
+	String reAccessToken(String userId, String refreshToken);
 }
