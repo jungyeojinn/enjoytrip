839ea4b9e10ddd0fb1f2d3e4b21a2b8f7a37f934
황성업 <wsup97@gmail.com>
2024-05-19 22:24:59 +0900

    feat: oathNaver에서 spring security 사용하려다 실패...

---
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/Oauth/controller/OauthController.java
@@ -0,0 +1,27 @@
+package com.ssafy.trip.Oauth.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+@RestController
+@RequestMapping("/oauth2")
+public class OauthController {
+
+    private static final Logger log = LoggerFactory.getLogger(OauthController.class);
+
+    @GetMapping("/login")
+    public ResponseEntity<?> authNaver(@RequestParam("code") String code, @RequestParam("state") String state){
+        return ResponseEntity.status(HttpStatus.OK).body(code+" "+state);
+    }
+
+
+
+}
---
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/Oauth/model/NaverOauthDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.trip.Oauth.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NaverOauthDto {
+
+}
