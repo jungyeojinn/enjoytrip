f08ad7167a6a030f3123f2016cd0911d9fba7933
tjrl1225 <tjrl1225@naver.com>
2024-05-20 11:58:45 +0900

    fix : application.properties에서 jwt 설정을 가져오지 못하는 문제 수정

--- enjoytrip_backend/src/main/java/com/ssafy/trip/EnjoytripKjhLjsJyjApplication.java
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/EnjoytripKjhLjsJyjApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.ssafy.trip.jwt.config.JwtProperties;
+
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.ssafy.trip" })
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackageClasses = {JwtProperties.class})
 public class EnjoytripKjhLjsJyjApplication {
 
 	public static void main(String[] args) {
