891e1fc173dc42130627bc85f363ff1c9897d30d
tjrl1225 <tjrl1225@naver.com>
2024-05-21 17:45:16 +0900

    feat : hotplace 로직이 올바른 interceptor 처리가 되도록 수정

--- enjoytrip_backend/src/main/java/com/ssafy/trip/config/WebMvcConfig.java
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/config/WebMvcConfig.java
@@ -30,7 +30,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final List<String> excludePointList = Arrays.asList(
 			"/user/login",
-			"/attraction/**",
+			"/oauth2/**",
 			"/swagger-resources/**",
             "/swagger-ui.html",
             "/swagger-ui/**",
