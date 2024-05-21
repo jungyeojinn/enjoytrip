6d27c393fa6658d4848df10d9c10dc045024aee4
김정훈 <j7papa@naver.com>
2024-05-21 12:21:58 +0900

    feat: pinia 프로필 연동

--- enjoytrip_frontend/src/components/AppHeader.vue
+++ enjoytrip_frontend/src/components/AppHeader.vue
@@ -1,28 +1,25 @@
 <script setup>
 import Profile from "@/components/common/Profile.vue";
+import { useUserStore } from '@/store/userStore';
+
+const userStore = useUserStore();
+
 </script>
 
 <template>
   <v-toolbar border color="#FFFFFF">
-    <v-container id="header-wrap" class="d-flex justify-space-between">
+    <v-container id="header-wrap" class="d-flex justify-space-between align-center">
       <nav class="navbar d-flex justify-space-between align-center">
         <router-link to="/" class="mr-10">
           <img src="/src/assets/logo.png" style="width: 80px" />
         </router-link>
-        <router-link to="/hotplace" exact-active-class="active-link"
-          >핫플레이스</router-link
-        >
-        <router-link to="/board" exact-active-class="active-link"
-          >게시판</router-link
-        >
-        <router-link to="/attraction-board" exact-active-class="active-link"
-          >여행지 리스트</router-link
-        >
-        <router-link to="/attraction" exact-active-class="active-link"
-          >여행지 탐색</router-link
-        >
+        <router-link to="/hotplace" exact-active-class="active-link">핫플레이스</router-link>
+        <router-link to="/board" exact-active-class="active-link">게시판</router-link>
+        <router-link to="/attraction-board" exact-active-class="active-link">여행지 리스트</router-link>
+        <router-link to="/attraction" exact-active-class="active-link">여행지 탐색</router-link>
       </nav>
-      <Profile />
+      <Profile v-if="userStore.isLogin" />
+      <router-link v-else to="/login" exact-active-class="active-link">로그인</router-link>
     </v-container>
   </v-toolbar>
 </template>
--- enjoytrip_frontend/src/components/common/Profile.vue
+++ enjoytrip_frontend/src/components/common/Profile.vue
@@ -1,23 +1,28 @@
+<script setup>
+import { useUserStore } from '@/store/userStore';
+
+const userStore = useUserStore();
+console.log(userStore.nickname);
+</script>
+
 <template>
   <v-container class="container">
     <v-row justify="center">
       <v-menu min-width="200px" rounded>
         <template v-slot:activator="{ props }">
           <v-btn icon v-bind="props">
-            <v-avatar color="brown">
-              <span>img</span>
+            <v-avatar>
+              <v-img src="/src/assets/default-avatar.png" cover></v-img>
             </v-avatar>
           </v-btn>
         </template>
         <v-card>
           <v-card-text>
             <div class="mx-auto text-center">
-              <v-avatar color="brown">
-                <span class="text-h5">{{ user.initials }}</span>
-              </v-avatar>
-              <h3>{{ user.fullName }}</h3>
+
+              <h3>{{ userStore.nickname }}</h3>
               <p class="text-caption mt-1">
-                {{ user.email }}
+                {{ userStore.id }}
               </p>
               <v-divider class="my-3"></v-divider>
               <v-btn variant="text" rounded> 회원 정보 수정 </v-btn>
@@ -30,17 +35,6 @@
     </v-row>
   </v-container>
 </template>
-<script>
-export default {
-  data: () => ({
-    user: {
-      initials: "JD",
-      fullName: "John Doe",
-      email: "john.doe@doe.com",
-    },
-  }),
-};
-</script>
 
 <style scoped>
 .container {
