1d85628bd42d503f4bbd6876218865bdf443e42b
정여진 <yeojin9905@naver.com>
2024-05-21 10:38:07 +0900

    feat: 이미지 삭제 및 업데이트

--- enjoytrip_backend/src/main/java/com/ssafy/trip/board/model/service/BoardService.java
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/board/model/service/BoardService.java
@@ -6,12 +6,13 @@ import java.util.List;
 import com.ssafy.trip.board.model.BoardDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface BoardService {
 	Page<BoardDto> boardList(Pageable pageable, int offset, int pageSize)  throws SQLException ;
 	BoardDto getBoard(int id)  throws SQLException ;
-	void registBoard(BoardDto board) throws SQLException ;
-	void updateBoard(BoardDto board) throws SQLException ;
+	void registBoard(BoardDto board, MultipartFile img) throws SQLException ;
+	void updateBoard(BoardDto board, MultipartFile img) throws SQLException ;
 	void deleteBoard(int id) throws SQLException ;
 	void deleteBoards(int[] ids)throws SQLException ;
 	
--- enjoytrip_backend/src/main/java/com/ssafy/trip/board/model/service/BoardServiceImpl.java
+++ enjoytrip_backend/src/main/java/com/ssafy/trip/board/model/service/BoardServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ssafy.trip.board.model.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,68 +15,113 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.trip.board.model.BoardDto;
 import com.ssafy.trip.board.model.mapper.BoardMapper;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class BoardServiceImpl implements BoardService {
-	
-	BoardMapper boardMapper;
-	@Autowired
-	public BoardServiceImpl(BoardMapper boardDao) {
-		super();
-		this.boardMapper = boardDao;
-	}
 
-	@Override
-	public Page<BoardDto> boardList( Pageable pageable, int pageNum, int pageSize) throws SQLException {
-		BoardDto board = new BoardDto();
-		int offset=pageNum*pageSize;
-		List<BoardDto> content = boardMapper.boardList(offset, pageSize);
+    BoardMapper boardMapper;
 
-		int total = boardMapper.getListBoardCount(board);
-		return new PageImpl<>(content, pageable, total);
-	}
+    @Autowired
+    public BoardServiceImpl(BoardMapper boardDao) {
+        super();
+        this.boardMapper = boardDao;
+    }
 
+    @Transactional(readOnly = true)
+    @Override
+    public Page<BoardDto> boardList(Pageable pageable, int pageNum, int pageSize) throws SQLException {
+        BoardDto board = new BoardDto();
+        int offset = pageNum * pageSize;
+        List<BoardDto> content = boardMapper.boardList(offset, pageSize);
 
+        int total = boardMapper.getListBoardCount(board);
+        return new PageImpl<>(content, pageable, total);
+    }
 
-	@Override
-	public BoardDto getBoard(int id) throws SQLException {
-		return boardMapper.getBoard(id);
-	}
+    @Transactional(readOnly = true)
+    @Override
+    public BoardDto getBoard(int id) throws SQLException {
+        return boardMapper.getBoard(id);
+    }
 
-	@Override
-	public void registBoard(BoardDto board) throws SQLException {
-		boardMapper.registBoard(board);
-	}
+    @Transactional
+    @Override
+    public void registBoard(BoardDto board, MultipartFile img) throws SQLException {
+        String imgPath = "";
+        if (img != null && !img.isEmpty()) imgPath = saveImage(img);
+        board.setImg(imgPath);
+        boardMapper.registBoard(board);
+    }
 
-	@Override
-	public void updateBoard(BoardDto board) throws SQLException {
-		boardMapper.updateBoard(board);
-	}
+    private String saveImage(MultipartFile img) {
+        String uuid = UUID.randomUUID().toString();    //파일 이름 중복 방지
+        String savedFilename = uuid;
 
-	@Override
-	public void deleteBoard(int id) throws SQLException {
-		boardMapper.deleteBoard(id);
-	}
+        String savedPath = "C:/upload/" + savedFilename;
 
-	@Override
-	public void deleteBoards(int[] ids) throws SQLException {
-		// ids 없을 때 체크
-		if(ids.length==0 || ids==null) throw new SQLException();
-		for (int i = 0; i < ids.length; i++) {
-			deleteBoard(ids[i]);
-		}
-	}
+        File file = new File(savedPath);
 
-	@Override
-	public void updateHit(int id) throws SQLException {
-		boardMapper.updateHit(id);
-	}
+        try {
+            img.transferTo(file);
+            return savedFilename;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	@Override
-	@Transactional
-	public BoardDto getBoardWithHit(int id) throws SQLException {
-		updateHit(id);
-		return getBoard(id);
-	}
-	
+    private void deleteImage(String imgPath) {
+        File fileToDelete = new File("C:/upload/" + imgPath);
+        boolean success = fileToDelete.delete();
+    }
+
+    @Transactional
+    @Override
+    public void updateBoard(BoardDto board, MultipartFile img) throws SQLException {
+        String originPath="";
+        if (img != null && !img.isEmpty()){
+            originPath=boardMapper.getImg(board.getId());
+            if(!originPath.equals("")){
+                deleteImage(originPath);
+            }
+            String imgPath = saveImage(img);
+            board.setImg(imgPath);
+        }
+        boardMapper.updateBoard(board);
+    }
+
+    @Transactional
+    @Override
+    public void deleteBoard(int id) throws SQLException {
+        String originPath=boardMapper.getImg(id);
+        if(!originPath.equals("")){
+            deleteImage(originPath);
+        }
+        boardMapper.deleteBoard(id);
+    }
+
+    @Transactional
+    @Override
+    public void deleteBoards(int[] ids) throws SQLException {
+        // ids 없을 때 체크
+        if (ids.length == 0 || ids == null) throw new SQLException();
+        for (int i = 0; i < ids.length; i++) {
+            deleteBoard(ids[i]);
+        }
+    }
+
+    @Transactional
+    @Override
+    public void updateHit(int id) throws SQLException {
+        boardMapper.updateHit(id);
+    }
+
+    @Override
+    @Transactional
+    public BoardDto getBoardWithHit(int id) throws SQLException {
+        updateHit(id);
+        return getBoard(id);
+    }
+
 }
